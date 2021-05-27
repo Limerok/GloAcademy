f781a6b363d4dacd397cc5acf2e39e5e51a8c8f1
Alexey Slautin <slaytin2014@yandex.ru>
2021-05-27 18:59:17 +0300

    Первый комит

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let money = 50000;
+let income = 200000;
+let addExpenses = 40000;
+let deposit = 40000;
+let mission = 'Выучить JavaScript';
+let period = 7;
+
+alert(mission + ' за ' + period + ' недель');
+console.log('Зарабатывать от ' + income + 'руб. ' + ' в месяц');
+console.log('Увеличить заработок до ' + (income + income) + 'руб. ' + ' в месяц');
