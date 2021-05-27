d63901b8b441dbc8964ecbf67a5f53f0bfbb9168
Alexey Slautin <slaytin2014@yandex.ru>
2021-05-27 20:31:57 +0300

    Сделал первое ДЗ

--- js/script.js
+++ js/script.js
@@ -6,5 +6,6 @@ let mission = 'Выучить JavaScript';
 let period = 7;
 
 alert(mission + ' за ' + period + ' недель');
+alert(mission + ' за ' + period - 5 + ' недель');
 console.log('Зарабатывать от ' + income + 'руб. ' + ' в месяц');
 console.log('Увеличить заработок до ' + (income + income) + 'руб. ' + ' в месяц');
