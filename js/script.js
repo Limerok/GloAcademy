5fa80aee0ab13d325a761d282fd8010360c123a9
Alexey Slautin <slaytin2014@yandex.ru>
2021-05-28 15:44:12 +0300

    Использовал методы и свойства на практике

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-let money = 50000; //“Доход за месяц”
+let money = 60000; //“Доход за месяц”
 let income = 40000;//строка с дополнительными доходом (например: фриланс)
-let addExpenses = (500, 4000, 3000);//строка с перечислением дополнительных расходов
+let addExpenses = 'Интернет, Такси, Коммуналка, Бензин, Отдых';//строка с перечислением дополнительных расходов
 //через запятую (например: интернет, такси, коммуналка)
 let deposit = true;//любое булево значение
 let mission = 9000000;//любое число (Какую сумму хотите накопить)
 let period = 6;//число от 1 до 12 (месяцев)
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('"Период равен ' + period + ' месяцев"' + '\n"Цель заработать ' + mission + 'рублей"');
+console.log(addExpenses.toLowerCase().split(', '));
+
+let butgetDay = money / 30;//дневной бюджет
+
+console.log(butgetDay);
