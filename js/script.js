1e3c7608c7796a117bcbb0e18c7395771e78b66d
Алексей Слаутин <Slaytin2014@yandex.ru>
2021-06-06 19:27:12 +0300

    Подготовил проект для работы со страницей

--- js/script.js
+++ js/script.js
@@ -21,6 +21,8 @@ let appData = {
     expenses: {}, // Объект с обязательными расходами
     addExpenses: [],
     deposit: false, // есть ли депозит в банке
+    percentDeposit: 0, // процент по вкладу
+    moneyDeposit: 0, // сколько денег на вкладе
     mission: 900000, //Желаем накопить
     period: 6,//За какой период желаем накопить
     budget: +money,//Доход за месяц
@@ -28,16 +30,40 @@ let appData = {
     budgetMonth: 0, //Бюджет на месяц (Доход - обязательные расходы)
     expensesMonth: 0,//Сумма обязательных расходов в месяц
     asking: function() {
-        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-        appData.addExpenses = addExpenses.toLowerCase().split(', ');
+
+        if (confirm('Есть ли у Вас дополнительный заработок?')){
+            let itemIncome,
+            cashIncome;
+            do {
+                itemIncome = prompt('Название дополнительного заработка');
+            } while(isNamber(itemIncome));
+            do {
+                cashIncome = prompt('Сколько в месяц Вы на этом зарабатываете?');
+            } while (!isNamber(cashIncome));
+            
+            appData.income[itemIncome] = +cashIncome;
+        }
+
+        let addExpensesAsking;
+            do{
+                addExpensesAsking = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+            } while (isNamber(addExpensesAsking));
+        appData.addExpenses = addExpensesAsking.toLowerCase().split(', ');
         appData.deposit = confirm('Есть ли у вас депозит в банке?');//любое булево значение
 
         let questionSum,
             questionItem,
-            expensesAmonth = +prompt('Сколько у Вас статей расходов в месяц?') ;
+            expensesAmonth;
+            do {
+                expensesAmonth = +prompt('Сколько у Вас статей расходов в месяц?') ;
+            } while(!isNamber(expensesAmonth));
+
         for (let i = 0; i < expensesAmonth; i++) {
-            questionItem = prompt('Введите обязательную статью расходов №' + (i+1) );
-            do{
+            do {
+                questionItem = prompt('Введите обязательную статью расходов №' + (i+1) );
+            } while(isNamber(questionItem));
+
+            do {
                 questionSum = prompt('Во сколько это обойдется?');
             }
             while ( !isNamber(questionSum) ); 
@@ -76,7 +102,21 @@ let appData = {
         }
     },
     
-    
+    getInfoDeposit: function() {
+        if (appData.deposit) {
+            do{
+                appData.percentDeposit = prompt('Какой годовой процент?', 5);
+            } while (!isNamber(appData.percentDeposit));
+            do{
+                appData.moneyDeposit = prompt('Какая сумма у Вас на вкладе?', 140000);
+            } while (!isNamber(appData.moneyDeposit));
+        }
+    },
+
+    calcSavedMoney: function() {
+        return appData.budgetMonth * appData.period;
+    }
+
 };
 
 appData.asking();
@@ -85,9 +125,19 @@ appData.getBudget();
 appData.getTargetMonth();
 appData.getStatusIncome();
 
-console.log(appData);
-
-console.log('Наша программа включает в себя данные:');
+/* console.log('Наша программа включает в себя данные:');
 for (let key in appData) {
     console.log(key + ': ' + appData[key]);
+} */
+
+appData.getInfoDeposit();
+console.log(appData.calcSavedMoney(), appData.percentDeposit, appData.moneyDeposit );
+
+
+let arr = [];
+let i = 0;
+for (let item of appData.addExpenses) {
+    arr[i] = item[0].toUpperCase() + item.slice(1);
+    i++;
 }
+console.log(arr.join(', '));
